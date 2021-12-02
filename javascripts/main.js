227ab8ef3dc2ea1e63a4cedb1597c7925dd98eae
kvtvs <kata.sara-aho@metropolia.fi>
2021-12-02 12:38:51 +0200

    javascript add list to mainpage

---
+++ javascripts/main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const url = 'http://localhost:3000'
+
+const ul = document.querySelector('#list');
+
+// create the sales ad
+const createSalesAd = (plants) => {
+  ul.innerHTML = '';
+  plants.forEach((plant) => {
+    const img = document.createElement('img');
+    img.src = url + '/' + plant.filename;
+    img.alt = plant.name;
+    img.classList.add('resp');
+
+    const figure = document.createElement('figure').appendChild(img);
+
+    const name = document.createElement('h2');
+    name.innerHTML = plant.name;
+
+    const seller = document.createElement('p');
+    seller.innerHTML = `${plant.seller}`;
+
+    const description = document.createElement('p');
+    description.innerHTML = `${plant.description}`;
+
+    const price = document.createElement('p');
+    price.innerHTML = `${plant.price}€`;
+
+    const date = document.createElement('p');
+    date.innerHTML = `${plant.date}`;
+
+    const buyButton = document.createElement('button');
+    buyButton.innerHTML = 'Osta';
+    // TODO: function for buying something
+
+    const li = document.createElement('li');
+    li.classList.add('light-border');
+
+    li.appendChild(name);
+    li.appendChild(figure);
+    li.appendChild(seller);
+    li.appendChild(description);
+    li.appendChild(price);
+    li.appendChild(date);
+    li.appendChild(buyButton);
+    ul.appendChild(li);
+  });
+};
