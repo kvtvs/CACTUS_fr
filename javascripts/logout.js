87936372c562772fb0f296c86e93d29fb617c125
kvtvs <kata.sara-aho@metropolia.fi>
2021-12-14 22:14:58 +0200

    add logout

---
+++ javascripts/logout.js
@@ -0,0 +1,18 @@
+'use strict';
+const url = 'http://localhost:3000'; // change url when uploading to server
+const path = 'http://127.0.0.1:5500';
+
+(async () => {
+  try {
+    const response = await fetch(url + '/auth/logout');
+    const json = await response.json();
+    console.log(json);
+    // remove token
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('user');
+    alert('Olet kirjautunut ulos! Palaa takaisin kirjautumissivulle painamalla OK.');
+    location.href = path + '/login/login.html';
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
