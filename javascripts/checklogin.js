20ee020b09589894c81213caae3bccf65375b21e
minsku <minja98@hotmail.com>
2021-12-15 06:29:08 +0200

    stuff

--- javascripts/checklogin.js
+++ javascripts/checklogin.js
@@ -1,7 +1,8 @@
 (async () => {
   'use strict';
   const url = 'http://localhost:3000'; // change url when uploading to server
-  const path = 'http://127.0.0.1:5500';
+  //const path = 'http://127.0.0.1:5500';
+  const path = 'http://localhost:63342/CACTUS_fr';
 
   // check sessionStorage
   if (!sessionStorage.getItem('token') || !sessionStorage.getItem('user')) {
@@ -17,7 +18,7 @@
     };
     const response = await fetch(url + '/user/token', fetchOptions);
     if (!response.ok) {
-      location.href = path + 'logout.html';
+      location.href = path + '/logout.html';
     } else {
       const json = await response.json();
       sessionStorage.setItem('user', JSON.stringify(json.user));
---
+++ javascripts/search.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const url = 'http://localhost:3000'
+//const path = 'http://127.0.0.1:5500';
+const path = 'http://localhost:63342/CACTUS_fr';
+
+const plantList = document.getElementById('plantList');
+const searchBar = document.getElementById('searchBar');
+let plants = [];
+
+/*
+console.log(searchBar);
+searchBar.addEventListener('keyup', (e) => {
+  console.log(e.target.value);
+});
+
+ */
+
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filteredPlants = plants.filter((character) => {
+    return (
+        character.name.toLowerCase().includes(searchString) ||  //joku muu kuin character??
+        character.description.toLowerCase().includes(searchString)
+
+    );
+  });
+  displayCharacters(filteredPlants);
+  console.log(filteredPlants);
+});
+
+
+//HAE MYYNTI-ILMOITUS (AJAX CALL)
+const loadPlants = async () => {
+  try {
+    const response = await fetch(url + '/plant'); //tähän API url
+    plants = await response.json();
+    displayCharacters(plants);
+    console.log(plants)
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const displayCharacters = (characters) => {
+  const htmlString = characters
+  .map((character) => {
+    return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+  })
+  .join('');
+  plantList.innerHTML = htmlString;
+};
+
+loadPlants();
